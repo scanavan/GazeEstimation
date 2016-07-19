a708977c6176c8a4553f3740e072f548a64568bd
scanavan <scanavan@binghamton.edu>
2016-07-19 16:43:32 -0400

    Added Reader class. Doesn't do anything yet.

---
+++ include/NDARReader.h
@@ -0,0 +1,12 @@
+#ifndef NDAR_READER_H
+#define NDAR_READER_H
+
+class NDARReader
+{
+public:
+	NDARReader();
+
+private:
+
+};
+#endif
---
+++ source/NDARReader.cpp
@@ -0,0 +1,6 @@
+#include "NDARReader.h"
+
+NDARReader::NDARReader()
+{
+
+}
